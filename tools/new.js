7337e76df37651ed04a472a0cfe2967f74ea4e2e
xygodcyx <1323943635@qq.com>
2026-04-21 00:19:33 +0800

    add post

--- tools/new.js
+++ tools/new.js
@@ -5,7 +5,14 @@ const [dir, title] = process.argv.slice(2)
 
 if (!dir) {
   console.error(
-    '❌ 请指定目录，例如: npm run new tec "文章标题"'
+    '❌ 请指定目录，例如: npm run new tec "文章标题"',
+  )
+  process.exit(1)
+}
+
+if (!title) {
+  console.error(
+    '❌ 请指定标题，例如: npm run new tec "文章标题"',
   )
   process.exit(1)
 }
